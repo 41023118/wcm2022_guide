716c203d4356c4bf59ee5e00c5b74b300df9b06d
mdecourse <mdecourse@gmail.com>
2022-03-08 21:17:08 +0800

    加入建立 Python 3.10.2 示範流程的 Wink 檔案

--- downloads/pp10_tcc_wink/portable_python310_tcc.js
+++ downloads/pp10_tcc_wink/portable_python310_tcc.js
@@ -4,5 +4,26 @@
   buttonFrameLength: 5,
   buttonFrameOffset: 2,
   frameStops: {
+    "2": [
+      { type: "gotourl", rect: { x: 346, y: 183, width: 60, height: 24 }, target: "https://mde.tw/wcm2022_guide/content/Portable Python.html#pp310", window: "PortablePython310" },
+    ],
+    "23": [
+      { type: "gotourl", rect: { x: 906, y: 139, width: 60, height: 24 }, target: "https://mde.tw/wcm2022_guide/content/Portable Python.html#pp310", window: "PortablePython310" },
+    ],
+    "236": [
+      { type: "gotourl", rect: { x: 577, y: 229, width: 60, height: 24 }, target: "https://mde.tw/wcm2022_guide/content/Portable%20Python.html", window: "Portable Python" },
+    ],
+    "273": [
+      { type: "gotourl", rect: { x: 577, y: 229, width: 60, height: 24 }, target: "https://mde.tw/wcm2022_guide/content/Portable%20Python.html", window: "Portable Python" },
+    ],
+    "283": [
+      { type: "gotourl", rect: { x: 827, y: 307, width: 60, height: 24 }, target: "https://mde.tw/wcm2022_guide/content/Portable%20Python.html", window: "Portable Python" },
+    ],
+    "320": [
+      { type: "gotourl", rect: { x: 615, y: 104, width: 60, height: 24 }, target: "https://mde.tw/wcm2022_guide/content/Portable%20Python.html#pp310", window: "Portable Python" },
+    ],
+    "353": [
+      { type: "gotourl", rect: { x: 456, y: 366, width: 60, height: 24 }, target: "https://mde.tw/wcm2022_guide/downloads/gen_portable_python.py", window: "gen_portable_python.py" },
+    ],
   },
 };
